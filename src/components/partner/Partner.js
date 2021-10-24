d8e984f0ac665867d3977f64bd3db5c2a76463bd
RitaDyvnych <m.dyvnych@gmail.com>
2021-10-24 20:30:20 +0300

    добавлены стили к партнерке и "о нас"

--- src/components/partner/Partner.js
+++ src/components/partner/Partner.js
@@ -4,29 +4,29 @@ import style from './partner.module.css';
 
 export default function Partner() {
   return (
-    <>
+    <div className="Container">
       <h2 className={style.Title}>Для партнеров</h2>
       <p className={style.Text}>Если Вы занимаетесь <span className={style.Text__orange}>зоотоварами
       </span> или у Вас крупный <span className={style.Text__orange}>питомник</span>,
       можете связаться с нами и обсудить возможность предоставления
       оптовых скидок. Для этого воспользуйтесь любым удобным для Вас
       способом на странице контакты.</p>
-      <div className={style.Container}>
+      <div className={style.TextContainer}>
         <img src={PartnerDog} alt="партнерская собака" />
         <div>
-          <p className={style.Text__orange}>Преимущества работы с нами:</p>
+          <p className={style.List}>Преимущества работы с нами:</p>
           <ul>
-          <li>Выдаем сертификаты качества и ветеринарную форму №2</li>
-          <li>Качество наших лакомств завоевало положительные отзывы
+          <li className={style.List__item}>Выдаем сертификаты качества и ветеринарную форму №2</li>
+          <li className={style.List__item}>Качество наших лакомств завоевало положительные отзывы
             владельцев магазинов города Киева</li>
-          <li>Широкий, постоянно развивающийся ассортимент</li>
-          <li>Конкурентные цены</li>
+          <li className={style.List__item}>Широкий, постоянно развивающийся ассортимент</li>
+          <li className={style.List__item}>Конкурентные цены</li>
         </ul>
-        <p>Для подробной информации Вы можете заполнить нашу он-лайн
+          <p className={style.List__item}>Для подробной информации Вы можете заполнить нашу он-лайн
           форму или связаться по телефону</p>
-        <p className={style.Text__orange}>Приглашаем к сотрудничеству мясокомбинаты и скотобойни</p>
+        <p className={style.List}>Приглашаем к сотрудничеству мясокомбинаты и скотобойни</p>
         </div>
       </div>
-    </>
+    </div>
   );
 }
